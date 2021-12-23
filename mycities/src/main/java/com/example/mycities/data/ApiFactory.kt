d4ef4f47e2d858ba79b57ebf616a6dcaf932374e
Juan Bocanegra <jbocane6@gmail.com>
2021-12-22 22:09:55 -0500

    [UPDATE] project

--- mycities/src/main/java/com/example/mycities/data/ApiFactory.kt
+++ mycities/src/main/java/com/example/mycities/data/ApiFactory.kt
@@ -1,7 +1,6 @@
 package com.example.mycities.data
 
 import okhttp3.OkHttpClient
-import okhttp3.internal.http.RetryAndFollowUpInterceptor
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
